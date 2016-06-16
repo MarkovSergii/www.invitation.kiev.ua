12e510c923225afc85abf55a64e2264c106bc57d
Markov Sergii <mr_sv@mail.ru>
2016-06-16 16:54:48 +0300

    ttt2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = require('./app');
 console.log("Hello!");
 
 
-
+console.log("555");
 
 app.listen(process.env.PORT || 3000, process.env.IP || 'localhost', function() {
     console.log('Server started at 127.0.0.1:3000 try to work!!!');
