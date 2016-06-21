b09b94b96b6f754af70a70899a4f246b409c1aa8
Markov Sergii <mr_sv@mail.ru>
2016-06-21 09:51:15 +0300

    ttt2 (reverted from commit 8b5d51253d5cb5d8fccfbc12c34f8a7508833a84)

--- server.js
+++ server.js
@@ -2,12 +2,9 @@
  * Created by user on 15.06.2016.
  */
 var app = require('./app');
-console.log("Hello!");
 
-
-console.log("555");
 
 app.listen(process.env.PORT || 3000, process.env.IP || 'localhost', function() {
-    console.log('Server started at 127.0.0.1:3000 try to work!!!');
+    console.log('Server started at 127.0.0.1:3000');
 });
 
